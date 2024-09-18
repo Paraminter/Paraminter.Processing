5ec694329fe36dac09b1146f87c485b15e3e71a6
ErikWe <erikwestberg7@gmail.com>
2024-09-18 22:00:45 +0200

    Dont provide generic handlers

--- src/Core/Queries/IsProcessArgumentAssociationsCompletedQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Paraminter.Processors.Queries;
-
-internal sealed class IsProcessArgumentAssociationsCompletedQuery
-    : IIsProcessArgumentAssociationsCompletedQuery
-{
-    public static IIsProcessArgumentAssociationsCompletedQuery Instance { get; } = new IsProcessArgumentAssociationsCompletedQuery();
-
-    private IsProcessArgumentAssociationsCompletedQuery() { }
-}
